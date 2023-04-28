47f5b43b015243cc4854e66e7f35d11974390a7e
ShameOnYou <yannickdonders@hotmail.be>
2023-04-28 17:10:02 +0200

    add loader

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,15 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
+import { Loader } from "@react-three/drei";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-	<Canvas>
-		<Experience />
-	</Canvas>
+	<>
+		<Canvas>
+			<Experience />
+		</Canvas>
+		<Loader />
+	</>
 );
